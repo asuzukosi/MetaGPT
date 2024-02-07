2c14f153527168129d646b5e39b0642681424290
asuzukosi <keloasuzu@yahoo.com>
2024-02-07 04:35:12 +0000

    feat: add notes for revision of the project in future

--- metagpt/strategy/__init__.py
+++ metagpt/strategy/__init__.py
@@ -2,3 +2,5 @@
 # @Date    : 12/23/2023 4:51 PM
 # @Author  : stellahong (stellahong@fuzhi.ai)
 # @Desc    :
+
+# TODO: Note the planners used in MetaGPT seem instresting and should be included in the ideas i need to explore while looking at LLMCompiler and ControlLLM
